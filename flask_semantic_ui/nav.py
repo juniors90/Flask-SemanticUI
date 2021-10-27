870bc0f86d268d302eb9e3d075c26003b654e8c4
unknown <juandavid9a0@gmail.com>
2021-10-27 19:34:46 -0300

    first commit

---
+++ flask_semantic_ui/nav.py
@@ -0,0 +1,95 @@
+from hashlib import sha1
+from dominate import tags
+from visitor import Visitor
+
+
+class SemanticUIRenderer(Visitor):
+    def __init__(self, html5=True, id=None):
+        self.html5 = html5
+        self._in_dropdown = False
+        self.id = id
+
+    def visit_Navbar(self, node):
+        # create a navbar id that is somewhat fixed, but do not leak any
+        # information about memory contents to the outside
+        node_id = self.id or sha1(str(id(node)).encode()).hexdigest()
+
+        root = tags.nav() if self.html5 else tags.div(role='navigation')
+        root['class'] = 'ui fluid container'
+        
+        div_container = tags.div(_class='ui attached stackable menu')
+        cont = root.add(div_container)
+        # collapse button
+        # title may also have a 'get_url()' method, in which case we render
+        # a brand-link <a class="item"><i class="mail icon"></i> Messages </a>
+        bar = cont.add(tags.div(
+            _class='menu',
+            id=node_id,
+        ))
+        bar_list = bar.add(tags.a(_class='item'))
+
+        for item in node.items:
+            bar_list.add(self.visit(item))
+
+        return root
+
+    def visit_Text(self, node):
+        if not self._in_dropdown:
+            return tags.p(node.text, _class='item')
+        return tags.div(node.text, _class='item')
+
+    def visit_Link(self, node):
+        item = tags.li()
+        item.add(tags.a(node.text, _class='item', href=node.get_url()))
+
+        return item
+
+    def visit_Separator(self, node):
+        if not self._in_dropdown:
+            raise RuntimeError('Cannot render separator outside Subgroup.')
+        return tags.li(node.text, role='separator', _class='ui divider')
+
+    def visit_Subgroup(self, node, icon='home icon'):
+        if not self._in_dropdown:
+            li = tags.div(_class="ui simple dropdown item")
+            a = tags.a(node.title, href='#', _class='item')
+            menu_dropdown = tags.div(_class="ui simple dropdown item")
+            menu_dropdown.add(a)
+            a['data-toggle'] = 'dropdown'
+            a['role'] = 'button'
+            a['aria-haspopup'] = 'true'
+            a['aria-expanded'] = 'false'
+            a.add(tags.span(_class='caret'))
+            if node.active:
+                a['class'] = 'ui active item'
+            
+            li.add(tags.i(_class='dropdown icon'))
+            ul = li.add(menu_dropdown.add(a.add(tags.i(_class=icon))))
+
+            self._in_dropdown = True
+            for item in node.items:
+
+                ul.add(self.visit(item))
+            self._in_dropdown = False
+
+            return li
+        else:
+            raise RuntimeError('Cannot render nested Subgroups')
+
+    
+    def visit_View(self, node):
+        menu = tags.div(_class='ui attached stackable menu')
+        item = tags.a(node.text, href=node.get_url(), title=node.text, _class='item')
+        menu.add(item)
+        if node.active:
+            item['class'] = 'ui active item'
+
+    
+    def visit_footer_View(self, node):
+        menu = tags.div(_class='ui link list')
+        item = tags.a(node.text, href=node.get_url(), title=node.text, _class='item')
+        item.add(item)
+        if node.active:
+            item['class'] = 'ui active item'
+
+        return item
---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   Flask-SemanticUI Project (https://github.com/juniors90/Flask-SemanticUI/).
+# Copyright (c) 2022, Milagros Colazo
+# License: MIT
+#   Full Text: https://github.com/juniors90/Flask-SemanticUI/blob/master/LICENSE
+
+# =====================================================================
+# DOCS
+# =====================================================================
+
+"""This file is for distribute and install Flask-SemanticUI"""
+
+# ======================================================================
+# IMPORTS
+# ======================================================================
+
+import os
+import pathlib
+
+from setuptools import setup  # noqa
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+REQUIREMENTS = ['Flask>=0.8', 'dominate>=2.6.0', 'visitor>=0.1.3']
+
+with open(PATH / "flask_semantic_ui" / "__init__.py") as fp:
+    for line in fp.readlines():
+        if line.startswith("__version__ = "):
+            VERSION = line.split("=", 1)[-1].replace('"', "").strip()
+            break
+
+
+with open("README.md") as fp:
+    LONG_DESCRIPTION = fp.read()
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+setup(
+    name="Flask-SemanticUI",
+    version=VERSION,
+    description="Implementation of Semantic-UI in Flask",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Ferreira Juan David",
+    author_email="juandavid9a0@gmail.com",
+    url="https://github.com/juniors90/Flask-SemanticUI",
+    packages=['flask_semantic_ui'],
+    include_package_data=True,
+    platforms='any',
+    license="The MIT License",
+    install_requires=REQUIREMENTS,
+    keywords=["Semantic-UI", "Flask", "Framework CSS"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
