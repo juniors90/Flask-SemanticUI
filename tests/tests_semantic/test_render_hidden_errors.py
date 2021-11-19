1706a4062ce519339641013636c8b7e64468ed38
unknown <juandavid9a0@gmail.com>
2021-11-19 01:10:51 -0300

    add new file->tests/tests_semantic/test_render_hidden_errors.py

---
+++ tests/tests_semantic/test_render_hidden_errors.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file was part of Flask-Bootstrap and was modified under the terms of
+# its BSD License. Copyright (c) 2013, Marc Brinkmann. All rights reserved.
+#
+# This file was part of Bootstrap-Flask and was modified under the terms of
+# its MIT License. Copyright (c) 2018 Grey Li. All rights reserved.
+#
+# This file is part of the
+# Flask-SemanticUI Project (https://github.com/juniors90/Flask-SemanticUI/).
+# Copyright (c) 2021, Ferreira Juan David
+# License: MIT
+# Full Text: https://github.com/juniors90/Flask-SemanticUI/blob/master/LICENSE
+
+from flask import render_template_string
+from flask_wtf import FlaskForm
+from wtforms import HiddenField, SubmitField
+from wtforms.validators import DataRequired
+
+
+def test_form_errors(app, client):
+    class TestForm(FlaskForm):
+        hide = HiddenField(
+            "Hide", validators=[DataRequired("Hide field is empty.")]
+        )
+        submit = SubmitField()
+
+    @app.route("/error", methods=["GET", "POST"])
+    def error():
+        form = TestForm()
+        if form.validate_on_submit():
+            pass
+        return render_template_string(
+            """
+        {% from 'semantic/wtf.html' import form_field, form_errors %}
+        <form method="post">
+            {{ form.hidden_tag() }}
+            {{ form_errors(form) }}
+            {{ form_field(form.submit) }}
+        </form>
+        """,
+            form=form,
+        )
+
+    response = client.post("/error", follow_redirects=True)
+    data = response.get_data(as_text=True)
+    assert "Hide field is empty." in data
